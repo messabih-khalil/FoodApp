3ebfa43d7f870a3c4f749e137ced895ee55419e7
messabih <msbih.khalil@gmail.com>
2023-11-25 01:27:40 +0100

    style(page.jsx): refactor page component to improve code readability and formatting

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,27 +1,21 @@
+'use client';
 
-"use client"
-
-import React from 'react'
-import Nav from './components/nav'
-import Hero from './components/Hero'
-import Side  from './components/Side'
+import React from 'react';
+import Nav from './components/nav';
+import Hero from './components/Hero';
+import Side from './components/Side';
 const page = () => {
-  return (
-<div className='flex justify-center w-full gap-3'>
+    return (
+        <div className='flex w-full gap-3'>
+            <div className='w-full m-5 pr-[350px]'>
+                <Nav />
+                <Hero />
+            </div>
+            <div className='border-solid h-screen w-[350px] fixed right-0 top-0'>
+                <Side />
+            </div>
+        </div>
+    );
+};
 
-  <div className=" w-3/4 m-5">
-    <Nav />
-    <Hero/>
-  </div>
-  <div className="w-1/4  border-l-2 border-orange-950 border-solid m-3 ">
-    <Side />
-  </div>
-</div>
-
-
-
-
-  )
-}
-
-export default page
+export default page;
