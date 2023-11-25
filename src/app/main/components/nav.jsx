c1fd38215c31f536f5d1e51d8e5c1a1a442bbc89
messabih <msbih.khalil@gmail.com>
2023-11-25 01:26:27 +0100

    style(nav.jsx): format code for better readability and consistency feat(nav.jsx): add table images and dynamically generate table components for available tables

--- src/app/main/components/nav.jsx
+++ src/app/main/components/nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
     Dialog,
     DialogContent,
@@ -6,68 +6,58 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-  } from "@/components/ui/dialog"
-  import { Button } from "@/components/ui/button" 
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import { faPizzaSlice} from "@fortawesome/free-solid-svg-icons";
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faBoxesPacking,
+    faPizzaSlice,
+} from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import TableImage from '@/assets/images/table.png';
+
+// =======
 export default function nav() {
-  return (
-    <div  className='flex w-full gap-x-11 m-5'> 
-     <div  className='w-4/5 flex gap-1'>
-     <FontAwesomeIcon
-        icon={faPizzaSlice}
-      />
-     <h1 className=' font-bold '> Pizza Swift </h1>
-     
-     </div>
+    return (
+        <div className='flex w-full gap-x-11 m-5 justify-between items-center pr-12'>
+            <div className='flex gap-2 items-center text-2xl'>
+                <FontAwesomeIcon icon={faPizzaSlice} />
+                <h1 className=' font-bold '> Pizza Swift </h1>
+            </div>
 
-      <Dialog className="w-1/6">
-      <DialogTrigger className='bg-[#FFD099] rounded-md p-2 font-medium '>
-     To Tables
-      </DialogTrigger>
-      <DialogContent>
-     <DialogHeader>
-      <DialogTitle className=" flex justify-center w-full m-3"> Available Tables</DialogTitle>
-      <DialogDescription className="grid grid-cols-3 gap-2 ">
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black '>Table 1 </p>
-  </div>
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black '>Table 2 </p>
-  </div>
+            <Dialog>
+                <DialogTrigger className='bg-[#FFD099] rounded-md py-2 px-5 font-medium space-x-2'>
+                    <FontAwesomeIcon icon={faBoxesPacking} />
+                    <span>To Tables</span>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle className=' flex justify-center w-full m-3'>
+                            {' '}
+                            Available Tables
+                        </DialogTitle>
+                        <DialogDescription className='grid grid-cols-3 gap-2 '>
+                            {[1, 2, 3, 4, 5, 6, 7, 8].map((e) => (
+                                <div
+                                    className=' flex items-center flex-col pb-1 rounded-xl border-[#FFD099] border-2'
+                                    key={e}
+                                >
+                                    <Image
+                                        src={TableImage}
+                                        alt='Picture of the author'
+                                        width={100}
+                                        height={100}
+                                    />
 
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black '>Table 3 </p>
-  </div>
-
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black '>Table 4 </p>
-  </div>
-
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black rou'>Table 5 </p>
-  </div>
-
-  <div className='bg-[#FF9209] w-20 h-11 '>
-    <p className='font-bold flex justify-center w-full align-middle
-     text-black '>Table 6 </p>
-  </div>
-
-
-
-</DialogDescription>
-
-     </DialogHeader>
-    </DialogContent>
-     </Dialog>
-    </div>
-  )
+                                    <p className='text-md font-bold'>
+                                        Table {e}
+                                    </p>
+                                </div>
+                            ))}
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 }
-
-
-
