b449cb40234fef3087353150e73c3789804c0596
messabih <msbih.khalil@gmail.com>
2023-11-26 09:30:45 +0100

    refactor(login page) : add some style

--- src/app/(login)/page.jsx
+++ src/app/(login)/page.jsx
@@ -1,9 +1,9 @@
-    'use client'
-    import React, { useState } from 'react';
-    import {Input} from '@/components/ui/input'
-    import { Button } from '@/components/ui/button'
+'use client';
+import React, { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
-    const LoginPage = () => {
+const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -16,48 +16,53 @@
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log('Username:', username);
-        console.log('Password:', password);
-
         setUsername('');
         setPassword('');
     };
 
     return (
         <div className='flex items-center justify-center h-screen bg-gray-100'>
-            <div className="bg-orange-200 p-10 rounded shadow-md">
-
-        <h1 className='text-2 font-semibold mb-4 flex items-center justify-center h-full'> Login </h1>
-        <form onSubmit={handleSubmit}>
-            <div className="mb-5 mt-5 text-sm">
-            <label htmlFor="username">Username:</label>
-            <Input
-                type="text"
-                id="username"
-                value={username}
-                onChange={handleUsernameChange}
-                required
-            />
+            <div className='bg-orange-200 p-5 rounded shadow-md w-[500px]'>
+                <h1 className='text-3xl font-semibold mb-4 flex items-center justify-center h-full'>
+                    {' '}
+                    Login{' '}
+                </h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className='space-y-3'
+                >
+                    <div className='space-y-2 w-full'>
+                        <p className='font-bold'>Username:</p>
+                        <Input
+                            type='text'
+                            id='username'
+                            value={username}
+                            onChange={handleUsernameChange}
+                            required
+                        />
+                    </div>
+                    <div className='text-sm space-y-2 '>
+                        <p className='font-bold'>Password:</p>
+                        <Input
+                            type='password'
+                            id='password'
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Button
+                            type='submit'
+                            className='bg-orange-500 text-white py-2 px-4 rounded-md w-full mt-4'
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </form>
             </div>
-            <div className="mb-5 text-sm">
-            <label htmlFor="password">Password:</label>
-            <Input
-                type="password"
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-            />
-            </div>
-            <div>
-            <Button type="submit" className="bg-orange-500 text-white py-2 px-4 rounded-md w-full">Login</Button>
-            </div>
-        </form>
-        </div>
         </div>
     );
-    };
+};
 
-    export default LoginPage;
+export default LoginPage;
