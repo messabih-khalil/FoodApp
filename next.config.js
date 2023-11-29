e2dec4ebcbe519e58f550f5914c8affe874b16f7
messabih <msbih.khalil@gmail.com>
2023-11-29 18:37:03 +0100

    chore(next.config.js): remove experimental configurations and unused webpack configurations
    The experimental configurations `appDir` and `serverComponentsExternalPackages` are removed as they are no longer needed. Additionally, the unused webpack configuration for `topLevelAwait` is also removed to simplify the configuration file.

--- next.config.js
+++ next.config.js
@@ -8,17 +8,6 @@ const nextConfig = {
             },
         ],
     },
-    experimental: {
-        appDir: true,
-        serverComponentsExternalPackages: ['mongoose'],
-    },
-    webpack(config) {
-        config.experiments = {
-            ...config.experiments,
-            topLevelAwait: true,
-        };
-        return config;
-    },
 };
 
 module.exports = nextConfig;
